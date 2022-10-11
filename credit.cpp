f5652c87c8320b6bc133df28807027441e71b976
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-10-10 20:20:32 -0400

    Update credit.cpp

--- credit.cpp
+++ credit.cpp
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+/*
+  ______ _____ _____
+ |  ____/ ____/ ____|
+ | |__ | (___| (___
+ |  __| \___ \\___ \
+ | |    ____) |___) |
+ |_|   |_____/_____/
+*/
+printf("Thank you for using FSS");
+printf("Made by Thecrazedpotatto on github");
+return 0
