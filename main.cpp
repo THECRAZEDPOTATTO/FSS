229c67e6405e7d0a77368fe24c2ec48450a1ea87
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-11-23 20:53:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,105 +12,22 @@
 #include <stdlib.h>
 #include "reader.h"
 using namespace std; 
-
+//NEW: Linux optimized
 int main()
 {
-	//os:HERE cmd
+	
 	int win10p = IsWindows10OrGreater();
 	int win7p = IsWindows7OrGreater();
 	int win8p = IsWindows8OrGreater();
 	int winvistap = IsWindowsVistaOrGreater();
 	int winxpp = IsWindowsXPOrGreater();
-	int server = IsWindowsServer();
-	string s = "os:win11";
-	int pos = s.find(":");
-	string sub = s.substr(pos + 1);
-	if (sub == "win10") {
-		cout << "Windows 10\n";
-		win10p = IsWindows10OrGreater();
-		if (win10p == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-
-	}
-	else if (sub == "win7") {
-		cout << "Windows 7\n";
-		win7p = IsWindows7OrGreater();
-		if (win7p == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-	}
-	else if (sub == "all") {
-		cout << "All";
-	}
-	else if (sub == "win8") {
-		cout << "Windows 8\n";
-		win8p = IsWindows8OrGreater();
-		if (win8p == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-	}
-	else if (sub == "winxp") {
-		cout << "Windows xp\n";
-		winxpp = IsWindowsXPOrGreater();
-		if (winxpp == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-	}
-	else if (sub == "winvista") {
-		cout << "Windows Vista\n";
-		winvistap = IsWindowsVistaOrGreater();
-		if (winvistap == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-
-	}
-	else if (sub == "winserver") {
-		cout << "Windows Server\n";
-		server = IsWindowsServer();
-		if (server == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-	}
-	else if (sub == "win11") {
-		cout << "Windows 11\n";
-		win7p = IsWindows7OrGreater();
-		if (win7p == 1) {
-			cout << "True";
-		}
-		else {
-			cout << "False";
-		}
-
-	}
-	else {
-		cout << "Not Valid Windows Type\n";
-	}														//PYTHON\\
+	int server = IsWindowsServer();			
 	//FILECOMPILER
 	bool mycore = false;
 	string mycompiler;
 	ifstream settingsfile("settings.fss");
 	while (getline(settingsfile, mycompiler)) {
 		if (mycompiler.find("main") != string::npos) { 
-			cout << mycompiler << endl;
 			string f = mycompiler;
 			int posf = f.find(":");
 			string subf = f.substr(posf + 1);
@@ -118,7 +35,6 @@ int main()
 			system(cmd.c_str());
 		}
 		if (mycompiler.find("pip") != string::npos) {
-			cout << mycompiler << endl;
 			string pip = mycompiler;
 			int pospip = pip.find(":");
 			string subpip = pip.substr(pospip + 1);
@@ -137,7 +53,6 @@ int main()
 			remove("pip.bat");
 		}
 		if (mycompiler.find("cmd") != string::npos) {
-			cout << mycompiler << endl;
 			string commandp = mycompiler;
 			int poscommandp = commandp.find(":");
 			string subcommandp = commandp.substr(poscommandp + 1);
@@ -145,14 +60,12 @@ int main()
 			system(code.c_str());
 		}
 		if (mycompiler.find("admin.main") != string::npos) {
-			cout << mycompiler << endl;
 			string f = mycompiler;
 			int posf = f.find(":");
 			string subf = f.substr(posf + 1);
 			ShellExecuteA(NULL, "runas", subf.c_str(), 0, 0, SW_SHOWNORMAL);
 		}
 		if (mycompiler.find("mov") != string::npos) {
-			cout << mycompiler << endl;
 			string f = mycompiler;
 			auto start = ":"s;
 			auto end = ">"s;
@@ -167,8 +80,6 @@ int main()
 			}
 			int posnew = f.find(">");
 			string subnew = f.substr(posnew + 1);
-			cout << matched+"\n";
-			cout << subnew+"\n";
 			ifstream infile(matched);
 			ofstream outfile(subnew);
 			string content = "";
@@ -180,14 +91,10 @@ int main()
 
 			content.erase(content.end() - 1);  
 			i--;
-
-			cout << i << " characters read...\n";
-
 			outfile << content;         
 			outfile.close();
 		}
 		if (mycompiler.find("write") != string::npos) {
-			cout << mycompiler << endl;
 			string ff = mycompiler;
 			auto startff = ":"s;
 			auto endff = ">"s;
@@ -202,8 +109,6 @@ int main()
 			}
 			int posnew = ff.find(">");
 			string subnew = ff.substr(posnew + 1);
-			cout << matched + "\n";
-			cout << subnew + "\n";
 			if (subnew == "newline") {
 				subnew = "\n";
 			}
@@ -213,7 +118,6 @@ int main()
 			newwriter.close();
 		}
 		if (mycompiler.find("awrite") != string::npos) {
-			cout << mycompiler << endl;
 			string ff = mycompiler;
 			auto startff = ":"s;
 			auto endff = ">"s;
@@ -228,8 +132,6 @@ int main()
 			}
 			int posnew = ff.find(">");
 			string subnew = ff.substr(posnew + 1);
-			cout << matched + "\n";
-			cout << subnew + "\n";
 			if (subnew == "newline") {
 				subnew = "\n";
 			}
@@ -239,11 +141,9 @@ int main()
 			newwriter.close();
 		}
 		if (mycompiler.find("brake") != string::npos) {
-			cout << mycompiler << endl;
 			this_thread::sleep_for(chrono::milliseconds(10000));
 		}
 		if (mycompiler.find("make-source") != string::npos) {
-			cout << mycompiler << endl;
 			string ff = mycompiler;
 			auto startff = ":"s;
 			auto endff = ">"s;
@@ -258,15 +158,19 @@ int main()
 			}
 			int posnew = ff.find(">");
 			string subnew = ff.substr(posnew + 1);
-			cout << matched + "\n";
-			cout << subnew + "\n";
 			execute("curl "+ subnew, matched);
 		}
 		if (mycompiler.find("openweb") != string::npos) {
 			cout << mycompiler << endl;
 		}
+        if (mycompiler.find("sleep") != string::npos) {
+            string sleep = mycompiler;
+			int possleep = sleep.find(":");
+			string subsleep = sleep.substr(possleep + 1);
+            int time = stoi(subsleep);
+            this_thread::sleep_for(chrono::seconds(time));
+		}
 		if (mycompiler.find("makefile") != string::npos) {
-			cout << mycompiler << endl;
 			string makefile = mycompiler;
 			int posmakefile = makefile.find(":");
 			string submakefile = makefile.substr(posmakefile + 1);
@@ -278,93 +182,7 @@ int main()
 			string subdel = del.substr(posdel + 1);
 			remove(subdel.c_str());
 		}
-		if (mycompiler.find("winos") != string::npos) {
-			cout << mycompiler << endl;
-			string s = mycompiler;
-			int pos = s.find(":");
-			string sub = s.substr(pos + 1);
-			if (sub == "win10") {
-				cout << "Windows 10\n";
-				win10p = IsWindows10OrGreater();
-				if (win10p == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-
-			}
-			else if (sub == "win7") {
-				cout << "Windows 7\n";
-				win7p = IsWindows7OrGreater();
-				if (win7p == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-			}
-			else if (sub == "all") {
-				cout << "All";
-			}
-			else if (sub == "win8") {
-				cout << "Windows 8\n";
-				win8p = IsWindows8OrGreater();
-				if (win8p == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-			}
-			else if (sub == "winxp") {
-				cout << "Windows xp\n";
-				winxpp = IsWindowsXPOrGreater();
-				if (winxpp == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-			}
-			else if (sub == "winvista") {
-				cout << "Windows Vista\n";
-				winvistap = IsWindowsVistaOrGreater();
-				if (winvistap == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-
-			}
-			else if (sub == "winserver") {
-				cout << "Windows Server\n";
-				server = IsWindowsServer();
-				if (server == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-			}
-			else if (sub == "win11") {
-				cout << "Windows 11\n";
-				win7p = IsWindows7OrGreater();
-				if (win7p == 1) {
-					cout << "True";
-				}
-				else {
-					MessageBox(NULL, L"Current OS Not Compatible ", L"ESS os Error", MB_ICONERROR | MB_OK);
-				}
-
-			}
-			else {
-				MessageBox(NULL, L"Not a Valid Windows OS", L"ESS os Error", MB_ICONERROR | MB_OK);
-			}
-		}
 		if (mycompiler.find("showcon") != string::npos) {
-			cout << mycompiler << endl;
 			string hide = mycompiler;
 			int poshide = hide.find(":");
 			string subhide = hide.substr(poshide + 1);
@@ -375,7 +193,7 @@ int main()
 				ShowWindow(Stealth, 0);
 			}
 			else {
-				cout << "False\n";
+				cout << "\n";
 			}
 		}
 	}
