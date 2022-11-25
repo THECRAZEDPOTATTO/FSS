715b0abddfd182786299d6a6508fa8d24edf5535
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-11-24 19:05:45 -0500

    Create messages.h

---
+++ FSS/messages.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#define debugm cout << "[" << "\033[1;36mDebug\033[0m" << "]"
+#define errorm cout << "[" << "\033[1;31mError\033[0m" << "]"//?Just messages are shown
