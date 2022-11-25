fab7c145133639c1adbfbd6a7513e64fe824e02c
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-11-25 17:47:53 -0500

    Update messages.h

--- FSS/messages.h
+++ FSS/messages.h
@@ -4,5 +4,7 @@ using namespace std;
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-#define debugm cout << "[" << "\033[1;36mDebug\033[0m" << "]"
+//#define debugm cout << "[" << "\033[1;36mDebug\033[0m" << "]" UNCOMMENT FOR DEVMODE
+//#define warnm cout << "[" << "\033[1;33mWarning\033[0m" << "]" UNCOMMENT FOR DEVMODE
+//#define compm cout << "[" << "\033[1;32mCompiled\033[0m" << "]" UNCOMMENT FOR DEVMODE
 #define errorm cout << "[" << "\033[1;31mError\033[0m" << "]"//?Just messages are shown
